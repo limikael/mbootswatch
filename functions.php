7827a44529f162c6ecc415ab3b450f4a3409585e
Mikael Lindqvist <li.mikael@gmail.com>
2021-06-27 06:50:29 +0800

    footer fix

--- functions.php
+++ functions.php
@@ -13,3 +13,16 @@ function mbootswatch_wp_enqueue_scripts() {
 }
 
 add_action("wp_enqueue_scripts","mbootswatch_wp_enqueue_scripts");
+
+function mbootswatch_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Footer', 'bootswatch' ),
+		'id' => 'footer',
+		'before_widget' => '<aside id="%1$s" class="widget col-xs-3 clearfix %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widgettitle">',
+		'after_title' => '</h3>',
+	) );
+}
+
+add_action( 'widgets_init','mbootswatch_widgets_init',11);
